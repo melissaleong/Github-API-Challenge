e43b2bbb82026c8481aae1b38ded8ba00ead9974
Melissa Leong <melissayleong@gmail.com>
2017-07-29 20:51:31 -0700

    Add json decoding

--- src/Main.elm
+++ src/Main.elm
@@ -2,28 +2,60 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput, onClick)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as JD
 
 
-{-
-   main =
-       Html.beginnerProgram { model = model, view = view, update = update }
--}
+-- import Json.Decode.Pipeline as JDP
+--import Json.Decode as Json exposing (..)
 ---- MODEL ----
 
 
+type alias User =
+    { name : String
+    , avatar_url : String
+    }
+
+
+decodeData : JD.Decoder User
+decodeData =
+    JD.map2 User
+        (JD.field "login" JD.string)
+        (JD.field "avatar_url" JD.string)
+
+
+getData : Http.Request User
+getData =
+    Http.get "https://api.github.com/users" decodeData
+
+
+
+--type AMsg
+--    = LoadData (Result Http.Error User)
+
+
+send : Cmd Msg
+send =
+    Http.send LoadData getData
+
+
 type alias Model =
-    { search : String }
+    { search : String
+    , hidden : Bool
+    , user : String -> String -> User
+    , image : String
+    }
 
 
 model : Model
 model =
-    Model ""
+    Model "" True User ""
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model "", Cmd.none )
+    ( Model "" True User "", Cmd.none )
 
 
 
@@ -33,6 +65,7 @@ init =
 type Msg
     = Search String
     | Submit
+    | LoadData (Result Http.Error User)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -42,6 +75,14 @@ update msg model =
             ( { model | search = search }, Cmd.none )
 
         Submit ->
+            ( { model | hidden = False }, send )
+
+        LoadData (Ok user) ->
+            ( { model | image = "https://avatars0.githubusercontent.com/u/1?v=4" }
+            , Cmd.none
+            )
+
+        LoadData (Err _) ->
             ( model, Cmd.none )
 
 
@@ -53,17 +94,19 @@ view : Model -> Html Msg
 view model =
     div []
         [ text "Search users names: "
-        , input [ onInput Search ] []
+        , input [ type_ "search", onInput Search ] []
         , div [] []
-        , button [ onClick Submit ] [ text "Submit" ]
+        , button [ onClick Submit ] [ text "SUBMIT" ]
+        , div [] []
+        , img [ src model.image ] []
         ]
 
 
 
 ---- PROGRAM ----
---   main : Program Never Model Msg
 
 
+main : Program Never Model Msg
 main =
     Html.program
         { view = view
